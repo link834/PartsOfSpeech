185ed806bfc4c8c33f800f0923a9bcb174db1c90
spencer <link834@gmail.com>
2014-06-07 17:36:00 -0600

    created driver an got the datafiles for the project

---
+++ Driver.py
@@ -0,0 +1,14 @@
+
+import sys
+import math
+import time
+
+def main():
+    # grab the first word of every document and give me probabilities
+    print "opening training file"
+    fileName = './assignment3/allTraining.txt'
+    training_file = open(fileName, 'r')
+    print training_file.readline()
+    
+if __name__ == '__main__':
+    main()
