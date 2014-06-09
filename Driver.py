5ff24bb476ed48cff007135713fc4be48c316f75
Spencer <link834@gmail.com>
2014-06-09 02:09:10 -0600

    updated Driver.py to calculate P(Z0)

--- Driver.py
+++ Driver.py
@@ -3,12 +3,36 @@ import sys
 import math
 import time
 
+#need to know
+# P( Z0 )
+# P( Xk | Zk )
+# P( Xk+1 | Zk ))
+
 def main():
-    # grab the first word of every document and give me probabilities
-    print "opening training file"
-    fileName = './assignment3/allTraining.txt'
-    training_file = open(fileName, 'r')
-    print training_file.readline()
+	
+	pZzero = {}
+
+	print "opening training file"
+	fileName = './assignment3/allTraining.txt'
+	training_data = open(fileName, 'r').readlines()
+
+	total_lines = len(training_data)
+	#print total_lines
+
+	for t_line in training_data:
+		t_pairs = t_line.split()
+		# used to find P( Z0 )
+		first_w, first_pos = t_pairs[0].split('_')
+
+		if(first_pos in pZzero):
+			pZzero[first_pos] += 1
+		else:
+			pZzero[first_pos] = 1
+
+		print "%s %d %f" % (first_pos, pZzero[first_pos], float(pZzero[first_pos]) / float(total_lines))
+		for t_pair in t_pairs:
+			word, pos = t_pair.split('_')
+
     
 if __name__ == '__main__':
-    main()
+	main()
